6441c946c66521964248ded9a033214a7e9500c2
LuanCalais <luancalais@gmail.com>
2022-08-20 11:31:39 -0300

    iniciar planejamento de design

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,12 +1,5 @@
 "use strict";
 exports.__esModule = true;
-//  Controle de mascara
-// const dateMask: any = IMask(document.getElementById('txtData'), {
-//     mask: Date,
-//     min: new Date(2000, 0, 1),
-//     max: new Date(2022, 08, 15),
-//     lazy: false
-// })
 // For Debug
 var log = function (text) {
     console.log(text);
@@ -14,15 +7,14 @@ var log = function (text) {
 // Função que irá limpar os valores
 function limpaValores(reg) {
     var _a;
-    log('Entrando em limpa valores');
     reg.data.trim().toLowerCase();
     reg.tipo.trim().toLowerCase();
     (_a = reg.descricao) === null || _a === void 0 ? void 0 : _a.trim().toLowerCase();
-    return reg;
+    var regJson = JSON.stringify(reg);
+    return regJson;
 }
 // Função que irá fazer a captura das informações do formulário
 var pegaValores = function (limpa) {
-    log('Entrando em pegaValores');
     var data = document.getElementById('txtData');
     var tipo = document.getElementById('txtTipo');
     var descricao = document.getElementById('txtDescricao');
@@ -33,10 +25,10 @@ var pegaValores = function (limpa) {
         descricao: descricao.value,
         valor: valor.value
     };
-    limpa(registro);
+    return limpa(registro);
 };
 // Inicio do fluxo que ira registrar os valores
 var registraValores = function () {
     log('Entrando no registra valores');
-    pegaValores(limpaValores);
+    var registro = pegaValores(limpaValores);
 };
--- assets/scripts/main.ts
+++ assets/scripts/main.ts
@@ -1,15 +1,6 @@
 import{registerI} from "./Interface/registroI"
 import {Register} from "./Class/register"
 
-//  Controle de mascara
-// const dateMask: any = IMask(document.getElementById('txtData'), {
-//     mask: Date,
-//     min: new Date(2000, 0, 1),
-//     max: new Date(2022, 08, 15),
-//     lazy: false
-// })
-
-
 // For Debug
 let log = (text:string | object) =>{
     console.log(text)
@@ -17,8 +8,6 @@ let log = (text:string | object) =>{
 
 // Função que irá limpar os valores
 function limpaValores (reg:registerI):string {
-
-    log('Entrando em limpa valores')
 
     reg.data.trim().toLowerCase()
     reg.tipo.trim().toLowerCase()
@@ -31,9 +20,7 @@ function limpaValores (reg:registerI):string {
 }
 
 // Função que irá fazer a captura das informações do formulário
-const pegaValores = (limpa: any):void => {
-
-    log('Entrando em pegaValores')
+const pegaValores = (limpa: any):string => {
 
     let data = document.getElementById('txtData') as HTMLElement
     let tipo = document.getElementById('txtTipo') as HTMLElement
@@ -47,12 +34,12 @@ const pegaValores = (limpa: any):void => {
         valor: valor.value 
     }
 
-    limpa(registro)
+    return limpa(registro)
 
 }
 
 // Inicio do fluxo que ira registrar os valores
 const registraValores = ():void => {
-    log('Entrando no registra valores')
-    pegaValores(limpaValores)    
+    log('Entrando no registra valores')  
+    let registro: string = pegaValores(limpaValores)
 }
